9d7e91ff7d5d8e7f576e2b1840627daedf446e5a
Marcel Körtgen <marcel.koertgen@gmail.com>
2018-06-03 09:06:44 +0200

    Change from scancode to license_finder

--- README.md
+++ README.md
@@ -1,13 +1,33 @@
 # license.automation
 
-Automated license scanning & dashboards for git repositories using
+Automated license scanning & dashboards for git repositories using already available
+license detection tools wrapped into a REST service.
 
-- [nexB/scancode-toolkit](https://github.com/nexB/scancode-toolkit) wrapped into a REST service.
+**Tools:**
+
+- [pivotal-legacy/LicenseFinder](https://github.com/pivotal-legacy/LicenseFinder)
+- [nexB/scancode-toolkit](https://github.com/nexB/scancode-toolkit)
 - ...
 
 ## Usage
 
 ```console
-curl -X POST http://localhost:5000 -H 'Content-Type: application/json' \
-  -d '{ "source_url": "https://github.com/awesome-inc/FontAwesome.Sharp.git", "commit_id": "66a957aa9f993ff8779bc3cdbd3ff2fe9d40d81e" }'
+curl -X POST \
+  http://localhost:5000 \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "source_url": "https://github.com/awesome-inc/neo4j-decorator.git",
+  "commit_id": "012abe3"
+}'
+```
+
+**Request format:**
+
+```json
+{
+  "source_url": "https://github.com/awesome-inc/neo4j-decorator.git",
+  "commit_id": "012abe3",
+  "format": "csv (default), json, html, markdown",
+  "install": "yarn (default), npm install, bundle install, nuget restore, ..."
+}
 ```
---
+++ license_finder/app.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python2.7
+
+from flask import Flask, request, Response
+from urlparse import urlparse
+import os
+import shutil
+import subprocess
+import tempfile
+
+CACHE_DIR = '/app/cache'
+DEFAULT_RESULT_FORMAT = 'csv' #'json'
+DEFAULT_INSTALL_CMD = 'yarn'
+
+if not os.path.exists(CACHE_DIR):
+    os.makedirs(CACHE_DIR)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def handle_get():
+    return display_help()
+
+
+@app.route('/', methods=['POST'])
+def scan_project():
+    if not request.is_json:
+        return "Post data must be json format"
+
+    request_data = request.get_json()
+
+    if 'source_url' not in request_data:
+        return display_help()
+    source_url = request_data['source_url']
+
+    if 'commit_id' not in request_data:
+        return display_help()
+    commit_id = request_data['commit_id']
+
+    format_param = DEFAULT_RESULT_FORMAT
+    if 'format' in request_data:
+        format_param = request_data['format']
+
+    install_packages_cmd = DEFAULT_INSTALL_CMD
+    if 'install' in request_data:
+        install_packages_cmd = request_data['install']
+
+    license_info_filename = CACHE_DIR + '/' + get_project_name(source_url) + "-" + commit_id \
+                            + "." + format_param
+    if not os.path.exists(license_info_filename):
+        git_repo_temp_dir, result = git_clone(source_url, commit_id)
+        if result != 0:
+            return "Error cloning git repo"
+        result = git_checkout(git_repo_temp_dir, commit_id)
+        if result != 0:
+            return "Error checking out commit"
+        install_dependencies(git_repo_temp_dir, install_packages_cmd)
+        find_licenses(git_repo_temp_dir, license_info_filename, format_param)
+        # TODO: save json report for filebeat (requires format: csv/json)
+        shutil.rmtree(git_repo_temp_dir, ignore_errors=True)
+
+    with open(license_info_filename, "r") as license_info_file:
+        license_info = license_info_file.readlines()
+
+    mimetype = 'text/plain'
+    if format_param == 'json' or format_param == 'json-pp':
+        mimetype = 'application/json'
+    elif format_param == 'html':
+        mimetype = 'text/html'
+
+    return Response(license_info, mimetype=mimetype)
+
+
+def display_help():
+    return 'Please submit a POST request with json content.  {"source_url":"<url>", "commit_id":"<commit>"}'
+
+
+def git_clone(source_url, commit):
+    project_name = get_project_name(source_url)
+    git_repo_dir = tempfile.mkdtemp(prefix=project_name, suffix="-" + commit)
+    result = subprocess.call(['git', 'clone', source_url, git_repo_dir])
+    return git_repo_dir, result
+
+
+def get_project_name(source_url):
+    parsed_url = urlparse(source_url)
+    _, project_name = os.path.split(parsed_url.path)
+    project_name = os.path.splitext(project_name)[0]
+    return project_name
+
+
+def git_checkout(repo_dir, commit):
+    return subprocess.call(['git', 'checkout', commit], cwd=repo_dir)
+
+
+def install_dependencies(source_dir, command):
+    return subprocess.call([command], cwd=source_dir)
+
+
+def find_licenses(source_dir, output_file, format_param):
+    # csv 2 json? https://stackoverflow.com/questions/19697846/how-to-convert-csv-file-to-multiline-json
+    cmd = "license_finder report --format={0} --save={1}".format( format_param, output_file)
+    return subprocess.call(['bash', '-lc', cmd], cwd=source_dir)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
