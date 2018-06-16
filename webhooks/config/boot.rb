8466bad961f60f97cca52cbefffe430ee42e3a72
mkoertgen <marcel.koertgen@gmail.com>
2018-06-16 16:32:28 +0200

    (webhooks) Workaround bootsnap/jruby issue

--- webhooks/config/boot.rb
+++ webhooks/config/boot.rb
@@ -1,4 +1,8 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
+# bootsnap broken on jruby, cf.: https://github.com/rails/rails/issues/32641
+unless defined? JRUBY_VERSION
+  require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+end
