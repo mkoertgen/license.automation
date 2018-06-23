169919f8e6c7417a5dd165a6716ba0d87e991b87
mkoertgen <marcel.koertgen@gmail.com>
2018-06-23 11:23:00 +0200

    (license_finder) Robustness

--- license_finder/app.py
+++ license_finder/app.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python2.7
-
-## print to console from flask route, cf. https://stackoverflow.com/a/33383239/2592915
-#from __future__ import print_function
-#import sys
-
 from flask import Flask, request, Response
-from urlparse import urlparse
 import os
 import shutil
 import subprocess
 import tempfile
+from urlparse import urlparse
 
-CACHE_DIR = '/app/cache'
-DEFAULT_RESULT_FORMAT = 'csv' #'json'
-DEFAULT_COLUMNS = 'package_manager name version homepage licenses license_links'
+LICENSE_CACHE_DIR = os.getenv('LICENSE_CACHE_DIR', '/app/cache')
+LICENSE_FORMAT = os.getenv('LICENSE_FORMAT', 'csv')
+LICENSE_COLUMNS = os.getenv('LICENSE_COLUMNS', 'package_manager name version homepage licenses license_links')
+LICENSE_DEBUG = os.getenv('LICENSE_DEBUG', 'false') == 'true'
 
-if not os.path.exists(CACHE_DIR):
-    os.makedirs(CACHE_DIR)
+if not os.path.exists(LICENSE_CACHE_DIR):
+    os.makedirs(LICENSE_CACHE_DIR)
 
 app = Flask(__name__)
 
@@ -31,36 +27,24 @@ def scan_project():
     if not request.is_json:
         return "Post data must be json format"
     request_data = request.get_json()
-
-    if 'source_url' not in request_data:
+    if not {'source_url', 'commit_id'} <= set(request_data):
         return display_help()
     source_url = request_data['source_url']
+    commit_id = request_data['commit_id']
 
-    commit_id = 'HEAD'
-    if 'commit_id' in request_data:
-      commit_id = request_data['commit_id']
-
-    report_fmt = DEFAULT_RESULT_FORMAT
+    report_fmt = LICENSE_FORMAT
     if 'format' in request_data:
         report_fmt = request_data['format']
 
-    columns = DEFAULT_COLUMNS
+    columns = LICENSE_COLUMNS
     if 'columns' in request_data:
         columns = request_data['columns']
 
-    license_info_filename = CACHE_DIR + '/' + get_project_name(source_url) + "-" + commit_id \
+    license_info_filename = LICENSE_CACHE_DIR + '/' + get_project_name(source_url) + "-" + commit_id \
                             + "." + report_fmt
     if not os.path.exists(license_info_filename):
         dir = git_fetch(source_url, commit_id)
-
-        if 'pre_tasks' in request_data:
-          #print(request_data['pre_tasks'], file=sys.stderr)
-          subprocess.check_call(request_data['pre_tasks'], cwd=dir)
-
-        find_licenses(dir, license_info_filename, report_fmt, columns)
-
-        # TODO: save json report for filebeat (requires format: csv/json)
-
+        find_licenses(dir, license_info_filename, report_fmt, LICENSE_COLUMNS)
         shutil.rmtree(dir, ignore_errors=True)
 
     with open(license_info_filename, "r") as license_info_file:
@@ -77,9 +61,9 @@ def display_help():
 def git_fetch(source_url, commit):
     project_name = get_project_name(source_url)
     git_repo_dir = tempfile.mkdtemp(prefix=project_name, suffix="-" + commit)
-    subprocess.check_call(['git', 'clone', source_url, git_repo_dir])
+    call(['git', 'clone', source_url, git_repo_dir])
     if commit != 'HEAD':
-        subprocess.check_call(['git', 'checkout', commit], cwd=git_repo_dir)
+        call(['git', 'checkout', commit], git_repo_dir)
     return git_repo_dir
 
 
@@ -91,12 +75,19 @@ def get_project_name(source_url):
 
 
 def find_licenses(source_dir, output_file, report_fmt, columns):
+    COMMAND = "license_finder report {3} --format={0} --save={1} --columns={2}"
     other_args = '--prepare-no-fail'
-    # csv 2 json? https://stackoverflow.com/questions/19697846/how-to-convert-csv-file-to-multiline-json
-    cmd = "license_finder report {3} --format={0} --save={1} --columns={2}".format( report_fmt, output_file, columns, other_args)
-    output = subprocess.check_output(['bash', '-lc', cmd], cwd=source_dir, stderr=subprocess.STDOUT)
+    args = COMMAND.format(report_fmt, output_file, columns, other_args)
+    result = call(['bash', '-lc', args], source_dir)
     if not os.path.exists(output_file):
-        raise Warning(output)
+        raise Warning(result)
+
+
+def call(cmd, dir='.'):
+    output = subprocess.check_output(cmd, cwd=dir, stderr=subprocess.STDOUT)
+    app.logger.info(output)
+    return output
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=LICENSE_FORMAT, host='0.0.0.0')
