622d871d5d0383c41ced3a0f75aa9ca479c5e459
Marcel Körtgen <marcel.koertgen@gmail.com>
2018-07-04 19:04:21 +0200

    (scanner) Cleanup

--- license_scanner/app/jobs/license_finder_job.rb
+++ license_scanner/app/jobs/license_finder_job.rb
@@ -7,43 +7,30 @@ class LicenseFinderJob < ApplicationJob
   ELASTIC_SEARCH_URL = ENV.fetch('ELASTIC_SEARCH_URL') { 'http://localhost:9200' }.freeze
 
   def perform(request)
-    url = request[:url]
-    commit = request[:commit]
-    csv = Scanner.call(url, commit)
-    body = bulk_for meta(request), csv
-    checked do
-      HTTP.headers(content_type: 'application/x-ndjson')
-          .post("#{ELASTIC_SEARCH_URL}/_bulk", body: body)
-    end
+    csv = Scanner.call(request[:url], request[:commit])
+    post_bulk csv_to_bulk(meta(request), csv)
   end
 
   private
 
-  def checked
-    r = yield
-    raise HTTP::ResponseError, "#{r.uri}: #{r}" unless r.status.success?
-    r
+  def csv_to_bulk(meta, csv)
+    bulk = []
+    index = "packages-#{Time.now.utc.strftime('%Y_%m_%d')}"
+    CSV.parse(csv) { |row| bulk += row_to_bulk(index, meta, row) }
+    bulk.join("\n") + "\n"
   end
 
-  def bulk_for(meta, csv)
+  def row_to_bulk(index, meta, row)
     # cf.: https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-bulk.html
-    index = "packages-#{Time.now.utc.strftime('%Y_%m_%d')}"
-    bulk = []
-    CSV.parse(csv) do |row|
-      id = SecureRandom.uuid
-      type, name, version, homepage, license, license_url = row
-      header = { index: { _index: index, _type: type, _id: id } }
-      bulk << header.to_json
-      doc = {
-        name: name,
-        version: version,
-        homepage: homepage,
-        license: { name: license, url: license_url }
-      }
-      doc = meta.merge doc
-      bulk << doc.to_json
-    end
-    bulk.join("\n") + "\n"
+    type, name, version, homepage, license, license_url = row
+    header = { index: { _index: index, _type: type, _id: SecureRandom.uuid } }
+    doc = {
+      name: name,
+      version: version,
+      homepage: homepage,
+      license: { name: license, url: license_url }
+    }
+    [header.to_json, meta.merge(doc).to_json]
   end
 
   def meta(request)
@@ -54,5 +41,11 @@ class LicenseFinderJob < ApplicationJob
       timestamp: Time.now.utc.iso8601,
       repository: { url: url.to_s, owner: owner, name: name }
     }
+  end
+
+  def post_bulk(body)
+    r = HTTP.headers(content_type: 'application/x-ndjson')
+            .post("#{ELASTIC_SEARCH_URL}/_bulk", body: body)
+    raise HTTP::ResponseError, "#{r.uri}: #{r}" unless r.status.success?
   end
 end
