aa79999d857ae4fe00a610dd74fbc83456ea733f
Marcel Körtgen <marcel.koertgen@gmail.com>
2018-07-06 13:06:11 +0200

    (provisioner) Update dashboard

--- provisioner/import.sh
+++ provisioner/import.sh
@@ -2,6 +2,6 @@
 set -e
 
 wget -T 30 --spider ${ES_URL}
-#elasticdump --input=/data/packages-2018_07_04.json --output=${ES_URL}/packages-2018_07_04 --type=data
+#elasticdump --input=/data/packages.json --output=${ES_URL}/packages-2018_07_06 --type=data
 elasticdump --input=/data/.kibana_mapping.json --output=${ES_URL}/.kibana --type=mapping
 elasticdump --input=/data/.kibana.json --output=${ES_URL}/.kibana --type=data
